992e3137ee5c5a94be6c039dd9443af4cc353769
Santos Odigie <santosodigie104@gmail.com>
2024-10-22 13:40:34 +0100

    set up word for users to guess and user lives

--- Program.cs
+++ Program.cs
@@ -1,2 +1,35 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+Greeting();
+
+string wordToGuess = "chelsea";
+int maxLives = 7;
+int currentLives = maxLives;
+bool win = false;
+
+List<char> guessedLetters = new List<char>();
+
+while (currentLives > 0 && !win)
+{
+    foreach (char c in wordToGuess)
+    {
+        if (guessedLetters.Contains(c))
+        {
+            Console.Write(c);
+        }
+        else
+        {
+            Console.Write("_ ");
+        }
+    }
+    Console.WriteLine("\nPlease guess a letter!");
+    Console.WriteLine(currentLives + "/" + maxLives + " lives remaining");
+
+}
+
+
+
+static void Greeting()
+{
+    Console.WriteLine("Hello welcome to my simple hangman project");
+}
+
